72dd60f7ed836248e28d22df8001d1861e7a47fb
ccrespoc4 <ccrespoc4@est.ups.edu.ec>
2025-10-25 14:20:36 -0500

    Configure Atlas MOngoDB

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose');
-const URI='mongodb://172.31.26.101:27017/usuarios_db';
+//const URI='mongodb://172.31.26.101:27017/usuarios_db';
+const URI='mongodb+srv://ccrespoc4_db_user:57SXE6VuJDzuLWps@msoft-ups.qbamrru.mongodb.net/?retryWrites=true&w=majority&appName=msoft-ups';
 mongoose.connect(URI)
 .then(db=> console.log('DB conectada'))
 .catch(err=> console.error(err));
